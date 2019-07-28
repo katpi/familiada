406f961e5848a8e30694fcd88bde4865a0295f0c
Katarzyna Piątkowska <kasia.m.piatkowska@gmail.com>
2019-07-28 16:52:51 +0200

    more progress onn settings

--- src/app/components/settings/edit-question-dialog/edit-question-dialog.component.ts
+++ src/app/components/settings/edit-question-dialog/edit-question-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { FamiliadaQuestion, FamiliadaResponse } from 'src/app/models/interfaces';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-question-dialog',
@@ -11,11 +12,23 @@ import { FamiliadaQuestion, FamiliadaResponse } from 'src/app/models/interfaces'
 export class EditQuestionDialog {
   answers: FamiliadaResponse[];
   displayedColumns: string[] = ['response', 'points'];
+  questionForm: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     public dialogRef: MatDialogRef<EditQuestionDialog>,
     @Inject(MAT_DIALOG_DATA) public data: FamiliadaQuestion,
   ) {
     this.answers = data.answers;
+    const formFields = {question: [data.question]};
+    this.answers.forEach(answer => {
+      formFields['response' + answer.id] = [answer.response];
+      formFields['points' + answer.id] = [answer.points];
+    });
+    this.questionForm = this.fb.group(formFields);
+  }
+
+  saveQuestion() {
+    console.log(this.questionForm.value);
   }
 }
